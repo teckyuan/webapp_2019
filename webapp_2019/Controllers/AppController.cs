0312cbb37b6d59afc81f97270601ab6c022e4ff5
teckyuan <teckyuan9898@hotmail.com>
2019-02-12 12:26:45 +0800

    sample page with base layout integrate

---
+++ webapp_2019/Controllers/AppController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapp_2019.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
